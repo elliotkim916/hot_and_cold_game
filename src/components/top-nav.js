5ec96ee1a55aa3035a52841c1684329c5a3f46bd
Elliot Kim <elliotkim916@gmail.com>
2018-04-05 16:31:15 -0700

    created top nav for learning about the game & starting new game

---
+++ src/components/top-nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function TopNav(props) {
+    return (
+        <nav>
+            <ul>
+                <li><a aria-label="How to play">What?</a></li>
+                <li><a>+ New Game</a></li>
+                <li><a>Hear state of game</a></li>
+            </ul>
+        </nav>
+    )
+}
