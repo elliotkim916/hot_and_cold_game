f90d0c957d3d5099955cf7afd83c9679bee11599
Elliot Kim <elliotkim916@gmail.com>
2018-04-05 19:12:23 -0700

    imported Header, GuessSection, StatusSection, & InfoSection to create & render Game

---
+++ src/components/game.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import Header from './header';
+import GuessSection from './guess-section';
+import StatusSection from './status-section';
+import InfoSection from './info-section';
+
+export default class Game extends React.component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            guesses: [],
+            feedback: 'Make your guess!',
+            auralStatus: '',
+            correctAnswer: Math.round(Math.random() * 100) + 1
+        };
+    }
+
+    restartGame() {
+        this.setState({
+            guesses: [],
+            feedback: 'Make your guess!',
+            auralStatus: '', 
+            correctAnswer: Math.round(Math.random() * 100) + 1
+        });
+    }
+
+    makeGuess(guess) {
+        guess = parseInt(guess, 10);
+        if (isNaN(guess)) {
+            this.setState({feedback: 'Please enter a valid number'});
+            return;
+        }
+
+        const difference = Math.abs(guess - this.state.correctAnswer);
+
+        let feedback;
+        if (difference >= 50) {
+            feedback = 'You\'re Ice Cold...';
+        } else if (difference >= 30) {
+            feedback = 'You\'re Cold...';
+        } else if (difference >= 10) {
+            feedback = 'You\'re Warm.';
+        } else if (difference >= 1) {
+            feedback = 'You\'re Hot!';
+        } else {
+            feedback = 'You got it!';
+        }
+
+        this.setState({
+            feedback,
+            guesses: [...this.state.guesses, guess]
+        });
+
+        document.title = feedback ? `${feedback} | Hot or Cold` : 'Hot or Cold';
+    }
+
+    render() {
+        const {feedback, guesses} = this.state;
+        const guessCount = guesses.length;
+
+        return (
+            <div>
+                <Header />
+                <main role="main">
+                    <GuessSection 
+                        feedback={feedback}
+                        guessCount={guessCount}
+                        onMakeGuess={guess => this.makeGuess(guess)}
+                    />
+                    <StatusSection 
+                        guesses={guesses}
+                    />
+                    <InfoSection />
+                </main>
+            </div>
+        );
+    }
+}
