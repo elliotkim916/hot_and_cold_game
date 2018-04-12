71908af691d8fa90b63857734d672b3dea0d0b6c
Elliot Kim <elliotkim916@gmail.com>
2018-04-11 17:53:07 -0700

    added assessment, guessCounter, & makeAGuess component

--- src/components/guess-section.js
+++ src/components/guess-section.js
@@ -4,11 +4,15 @@ import Feedback from './feedback';
 import GuessForm from './guess-form';
 
 export default function GuessSection(props) {
-    const { feedback, guessCount } = props;
+    const { evaluation, totalGuesses } = props;
+    // const evaluation = props.evaluation;
+    // const totalGuesses = props.totalGuesses;
     return (
         <section aria-label="Guess section" aria-describedby="feedback">
-            <Feedback feedback={feedback} guessCount={guessCount} />
-            <GuessForm onMakeGuess={guess => props.onMakeGuess(guess)} />
+            <Feedback assessment={evaluation} guessCounter={totalGuesses} />
+            <GuessForm makeAGuess={guess => props.onMakeGuess(guess)} />
+            {/* props.onMakeGuess() is the function being called from game.js, the prop being passed down from game.js, 
+                the prop value that is a function from game.js  */}
         </section>
     );
 }
