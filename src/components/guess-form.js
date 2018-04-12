caef19749abf94e23e060401538c2f4d5e392b5b
Elliot Kim <elliotkim916@gmail.com>
2018-04-11 18:02:43 -0700

    added comments & using ref to provide access to input element

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './guess-form.css';
 
 export default class GuessForm extends React.Component {
     onSubmit(event) {
         event.preventDefault();
 
-        if (this.props.onMakeGuess) {
+        if (this.props.makeAGuess) {
             const value = this.input.value;
-            this.props.onMakeGuess(value);
+            // this.props.makeAGuess() is the function being called, passing in the value
+            this.props.makeAGuess(value);
         }
         this.input.value = '';
         this.input.focus();
@@ -25,14 +27,10 @@ export default class GuessForm extends React.Component {
                     autoComplete="off"
                     aria-labelledby="feedback"
                     ref={input => (this.input = input)}
+                    // we use ref to provide access to the input element via this.input
                     required
                 />
-                <button
-                    type="submit"
-                    name="submit"
-                    id="guessButton"
-                    className="button"
-                >
+                <button type="submit" name="submit" id="guessButton" className="button">
                 Guess
                 </button>
             </form>
