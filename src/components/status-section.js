247d692e0f523960037cb6018473272257ec20a5
Elliot Kim <elliotkim916@gmail.com>
2018-04-11 18:01:16 -0700

    added totalCount and allGuesses props

--- src/components/status-section.js
+++ src/components/status-section.js
@@ -4,13 +4,14 @@ import GuessList from './guess-list';
 import GuessCount from './guess-count';
 
 export default function StatusSection(props) {
-    const {guesses} = props;
-    const guessCount = guesses.length;
+    const {myGuesses} = props;
+    // const myGuesses = props.myGuesses;
+    const guessCount = myGuesses.length;
 
     return (
         <section aria-labelledby="guessCount" aria-describedby="guessList">
-            <GuessCount guessCount={guessCount} />
-            <GuessList  guesses={guesses} />
+            <GuessCount totalCount={guessCount} />
+            <GuessList  allGuesses={myGuesses} />
         </section>
     );
 }
