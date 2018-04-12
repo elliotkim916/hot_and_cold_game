8e5c9513cf60bc015439ea08c98859dc0de46e19
Elliot Kim <elliotkim916@gmail.com>
2018-04-11 17:48:17 -0700

    added onClick prop within a tag in list item

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import './top-nav.css';
 
 export default function TopNav(props) {
     return (
         <nav>
-            <ul>
-                <li><a aria-label="How to play">What?</a></li>
-                <li><a>+ New Game</a></li>
-                <li><a>Hear state of game</a></li>
+            <ul className="clearfix">
+                <li>
+                    <a 
+                    aria-label="How to play" 
+                    href="#what" 
+                    className="what"
+                    >
+                        What?
+                    </a>
+                </li>
+                <li>
+                    <a
+                    aria-label="Start a new game"
+                    href="#feedback"
+                    className="new"
+                    onClick={() => props.newGame()}
+                >
+                    + New Game
+                    </a>
+                </li>
             </ul>
         </nav>
     )
